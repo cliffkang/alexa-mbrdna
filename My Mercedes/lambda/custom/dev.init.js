b3912c984fbb8b1388b90c1ee15963e864277656
Cliff Kang <inlovewithhim@gmail.com>
2018-08-21 15:51:03 -0700

    set up local test server using express + ngrok

---
+++ My Mercedes/lambda/custom/dev.init.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+
+const { handler } = require('./index');
+
+const server = express();
+server.use(express.json());
+
+// Create POST route
+server.post('/', (req, res) => {
+    // Create dummy context with fail and succeed functions
+    const context = {
+        fail: () => res.sendStatus(500),
+        succeed: data => res.send(data),
+    };
+
+    // Initialize alexa sdk
+    handler(req.body, context);
+});
+
+// Start express server
+server.listen(3000, () => {
+    console.log('Local alexa skill listening on port 3000!');
+});
